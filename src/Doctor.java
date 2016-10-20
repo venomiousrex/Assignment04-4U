755a2678b109e8319afc34cb6e76f5dc3b5e3fe3
Hadida Family <Hadida Family@192.168.2.38>
2016-10-20 19:49:37 -0400

    RYUU GA WAGA TEKI WO KURAU

--- src/Doctor.java
+++ src/Doctor.java
@@ -8,26 +8,49 @@
  * @author hadik9595
  */
 public class Doctor {
+
     private int row;
-    private int col; 
-    
-    public Doctor(int row, int col){
+    private int col;
+
+    public Doctor(int row, int col) {
         this.col = col;
         this.row = row;
-        
+
     }
-    
-    public void move(int newRow, int newCol){
+/**
+ * makes the doctor move on the board
+ * @param newRow takes the doctor's row number
+ * @param newCol takes the doctor's column number
+ */
+    public void move(int newRow, int newCol) {
+        int randomRow = (int) (Math.random() * 12);
+        int randomCol = (int) (Math.random() * 12);
         int rowMove = this.row - newRow;
         int colMove = this.col - newCol;
-        System.out.println(rowMove);
+        System.out.println("Row: " + rowMove);
+        System.out.println("Col: " + colMove);
+
+        if (rowMove <= 1 && rowMove >= -1 && colMove <= 1 && colMove >= -1) {
+            System.out.println("It works!");
+            this.col = newCol;
+            this.row = newRow;
+        }else{
+            System.out.println("RANDOMNESS OCCURR!");
+            this.col = randomCol;
+            this.row = randomRow;
+            
+        }
+        
+
+        System.out.println("Currently " + this.row + " and the col is " + this.col);
     }
-    public int getRow(){
+
+    public int getRow() {
         return this.row;
     }
-    public int getCol(){
+
+    public int getCol() {
         return this.col;
     }
-    
-    
+
 }
--- src/Game.java
+++ src/Game.java
@@ -16,30 +16,45 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int initialCol = 1;
+        int initialRow = 1;
+       
         //create board game
         Board board = new Board(12,12);
-        Doctor doctor = new Doctor(12,12);
+        Doctor doctor = new Doctor(initialCol,initialRow);
         Dalek dalek = new Dalek(3,3);
+        Dalek dalek2 = new Dalek(4,4);
         
         
         // put a coloured peg at row,col
         board.putPeg(Color.RED, 1,5);
         board.putPeg(Color.BLACK, 2,5);
         //remove peg
-        board.removePeg(1,5);
+       
         
         
-        doctor.move(4, 3);
+        
         //displays a message
         board.displayMessage("Please click the board");
         
-        while(true){
+        
         //get a click on board
-        Coordinate click = board.getClick();
+        while(true){
+         Coordinate click = board.getClick();
+         board.putPeg(Color.GREEN, initialRow,initialCol);
         int row = click.getRow();
         int col = click.getCol();
-        board.putPeg(Color.MAGENTA, row,col);
-        
+        initialCol = col;
+        initialRow = row;
+       
+        doctor.move(row, col);
+         int newRow = doctor.getRow();
+        int newCol = doctor.getCol();
+        board.putPeg(Color.GREEN, newRow,newCol);
+   
+
+       
         }
+        
     }
 }
